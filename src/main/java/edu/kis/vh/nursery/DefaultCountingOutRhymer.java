15790345d122bbeae286440651519dce12636d5b
nova <257472@edu.p.lodz.pl>
2026-03-25 06:03:35 +0100

    zaostrzenie heremetyzacji (private tam gdzie sie da)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
   private int[] number = new int[12];
 
-  public int total = -1;
+  private int total = -1;
 
   public void countIn(int in) {
     if (!isFull()) number[++total] = in;
