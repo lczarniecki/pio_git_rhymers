0237eb144a4ec7901d4295aa32903c0650c90910
nova <257472@edu.p.lodz.pl>
2026-04-14 02:19:18 +0200

    zad 17 wygenerowanie opisu klasy listy dwukierunkowej

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,23 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Implementacja dwukierunkowej listy jednokierunkowo używanej jako stos (LIFO).
+ *
+ * Klasa przechowuje elementy typu int w strukturze listy powiązanej za pomocą węzłów
+ * (Node), gdzie każdy węzeł posiada referencje do poprzedniego i następnego elementu.
+ *
+ * Lista jest używana głównie jako stos:
+ * - push() dodaje element na koniec listy (szczyt stosu),
+ * - pop() usuwa i zwraca ostatnio dodany element,
+ * - top() zwraca ostatni element bez jego usuwania.
+ *
+ * Struktura nie posiada ograniczenia rozmiaru (isFull() zawsze zwraca false).
+ * Pusta lista reprezentowana jest przez wartość last == null.
+ *
+ * W przypadku operacji pop() lub top() na pustej liście zwracana jest wartość EMPTY.
+ *
+ * Złożoność operacji push/pop/top wynosi O(1).
+ */
 public class IntLinkedList {
 	private class Node {
 
